5345bbd8bf145f7b5fd6b75256ce499f0a2e797a
Bartosz Janusz <bartosz.janusz@naturalantibody.com>
2025-10-17 18:33:55 +0200

    refactor: expose ALPHABET in __init__.py

--- proteinmpnn/__init__.py
+++ proteinmpnn/__init__.py
@@ -1,3 +1,14 @@
-from proteinmpnn.models import load_abmpnn, load_proteinmpnn, load_model
-from proteinmpnn.score import score_sequences, ScoringResult
+from proteinmpnn.featurize import ALPHABET
+from proteinmpnn.models import load_abmpnn, load_model, load_proteinmpnn
 from proteinmpnn.sample import sample
+from proteinmpnn.score import ScoringResult, score_sequences
+
+__all__ = [
+    "load_model",
+    "load_proteinmpnn",
+    "load_abmpnn",
+    "sample",
+    "score_sequences",
+    "ScoringResult",
+    "ALPHABET",
+]
