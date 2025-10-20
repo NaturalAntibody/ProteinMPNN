24d71ccdb6b4fd5c18a5be08ecd3eebb2b9964a9
Bartosz Janusz <bartosz.janusz@naturalantibody.com>
2025-10-20 17:18:29 +0200

    refactor: removed featurization from sample() to avoid parsing pdb multiple times
    BREAKING CHANGE: sample() no longer accepts pdb_path, use featurize_pdb() first

--- proteinmpnn/featurize.py
+++ proteinmpnn/featurize.py
@@ -41,34 +41,61 @@ def tied_featurize(*args, **kwargs) -> TiedFeaturizeResult:
     return TiedFeaturizeResult(*tied_featurize_orig(*args, **kwargs))
 
 
-def featurize_pdb(pdb: Path | TextIO, designed_chains: list[str], fixed_chains: list[str], device) -> TiedFeaturizeResult:
+def featurize_pdb(
+    pdb: Path | TextIO,
+    designed_chains: list[str],
+    fixed_chains: list[str],
+    device,
+    chain_designed_positions: dict | None = None,
+) -> TiedFeaturizeResult:
     all_chains = designed_chains + fixed_chains
-    protein = parse_pdb(pdb, chain_ids=all_chains)
-    chain_id_dict = {protein["name"]: (designed_chains, fixed_chains)}
-    return tied_featurize(batch=[protein], device=device, chain_dict=chain_id_dict)
+    structure = parse_pdb(pdb, chain_ids=all_chains)
+    return featurize_structure(
+        structure, designed_chains, fixed_chains, device, chain_designed_positions
+    )
 
 
 def featurize_structure(
-    structure: Structure, designed_chains: list[str], fixed_chains: list[str], device
+    structure: Structure,
+    designed_chains: list[str],
+    fixed_chains: list[str],
+    device,
+    chain_designed_positions: dict | None = None,
 ) -> TiedFeaturizeResult:
     all_chains = designed_chains + fixed_chains
     structure = select_chains(structure, all_chains)
     chain_id_dict = {structure["name"]: (designed_chains, fixed_chains)}
-    return tied_featurize(batch=[structure], device=device, chain_dict=chain_id_dict)
+    fixed_positions_dict = None
+    if chain_designed_positions is not None:
+        fixed_positions_dict = get_fixed_positions_dict(
+            structure, chain_designed_positions
+        )
+    return tied_featurize(
+        batch=[structure],
+        device=device,
+        chain_dict=chain_id_dict,
+        fixed_positions_dict=fixed_positions_dict,
+    )
 
 
 def encode_sequence(features: TiedFeaturizeResult, seq: str) -> TiedFeaturizeResult:
     input_seq_length = len(seq)
-    S_input = torch.tensor([ALPHABET_DICT[AA] for AA in seq], device=features.S.device)[None, :].repeat(
-        features.X.shape[0], 1
-    )
+    S_input = torch.tensor([ALPHABET_DICT[AA] for AA in seq], device=features.S.device)[
+        None, :
+    ].repeat(features.X.shape[0], 1)
     # assumes that S and S_input are alphabetically sorted for masked_chains
     features.S[:, :input_seq_length] = S_input
     return features
 
 
-def get_fixed_positions_dict(protein: dict, chain_designed_positions: dict[str, list[int]]) -> dict:
-    seq_chains = {key.replace("seq_chain_", ""): seq for key, seq in protein.items() if key.startswith("seq_chain_")}
+def get_fixed_positions_dict(
+    protein: dict, chain_designed_positions: dict[str, list[int]]
+) -> dict:
+    seq_chains = {
+        key.replace("seq_chain_", ""): seq
+        for key, seq in protein.items()
+        if key.startswith("seq_chain_")
+    }
     res = {}
     for chain_id, seq in seq_chains.items():
         all_positions = set(range(1, len(seq) + 1))
--- proteinmpnn/sample.py
+++ proteinmpnn/sample.py
@@ -1,50 +1,31 @@
-from pathlib import Path
-from typing import Optional, TextIO
 
 import numpy as np
 import torch
+from torch.nn import Module
 from tqdm import tqdm
 
-from proteinmpnn.config import RESULTS_DIR, DATA_DIR
-from proteinmpnn.featurize import ALPHABET, get_fixed_positions_dict, tied_featurize
-from proteinmpnn.io import parse_pdb
-from proteinmpnn.models import load_abmpnn
+from proteinmpnn.featurize import ALPHABET, TiedFeaturizeResult
 from proteinmpnn.protein_mpnn_utils import _S_to_seq
-from torch.nn import Module
 
 
 def sample(
     model: Module,
-    pdb: Path | TextIO,
-    designed_chains: list[str],
-    fixed_chains: list[str],
+    features: TiedFeaturizeResult,
     temperature: float,
     num_seq_per_target: int,
-    chain_designed_positions: Optional[dict] = None,
     device: torch.device = torch.device("cuda:0"),
 ):
-    all_chains = designed_chains + fixed_chains
-    protein = parse_pdb(pdb, chain_ids=all_chains)
-    chain_id_dict = {protein["name"]: (designed_chains, fixed_chains)}
-
-    fixed_positions_dict = None
-    if chain_designed_positions is not None:
-        fixed_positions_dict = get_fixed_positions_dict(protein, chain_designed_positions)
-
-    features = tied_featurize(
-        [protein],
-        device,
-        chain_id_dict,
-        fixed_positions_dict,
-    )
-
     # Generate some sequences
     omit_AAs = "X"
     omit_AAs_np = np.array([AA in omit_AAs for AA in ALPHABET]).astype(np.float32)
     bias_AAs_np = np.zeros(len(ALPHABET))
 
     results = []
-    for _ in tqdm(range(num_seq_per_target), total=num_seq_per_target, disable=num_seq_per_target == 1):
+    for _ in tqdm(
+        range(num_seq_per_target),
+        total=num_seq_per_target,
+        disable=num_seq_per_target == 1,
+    ):
         noise = torch.randn(features.chain_M.shape, device=device)
         sample_dict = model.sample(
             features.X,
@@ -72,35 +53,3 @@ def sample(
         results.append(seq)
 
     return results
-
-
-if __name__ == "__main__":
-
-    results_dir = RESULTS_DIR / "if_sampling"
-    results_dir = "/tmp"
-    # results_dir.mkdir(parents=True, exist_ok=True)
-    res = sample(
-        model=load_abmpnn(),
-        pdb=DATA_DIR / "1dqj.pdb",
-        designed_chains=["A", "B"],
-        fixed_chains=[],
-        temperature=0.1,
-        num_seq_per_target=1,
-        out_jsonl="/tmp/out.json",
-    )
-    print(len(res[1]["seq"]))
-
-    exit()
-    for temperature, n_seq in [(0.1, 10000), (0.2, 10000), (0.4, 2000), (0.6, 1000), (0.8, 1000), (1.0, 1000)]:
-        with open(results_dir / f"abmpnn_{temperature}.jsonl", "w") as out_jsonl:
-            sample(
-                load_abmpnn(),
-                DATA_DIR / "1N8Z_imgt.pdb",
-                ["B"],
-                [],
-                {"B": list(range(98, 108))},
-                temperature,
-                num_seq_per_target=n_seq,
-                batch_size=1,
-                out_jsonl=out_jsonl,
-            )
