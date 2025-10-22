3c9d02c7a9c44b4770226525acf809d8bb1c1581
Bartosz Janusz <bartosz.janusz@naturalantibody.com>
2025-10-22 16:32:31 +0200

    fix: fixed init.py

--- proteinmpnn/__init__.py
+++ proteinmpnn/__init__.py
@@ -1,14 +1,19 @@
 from proteinmpnn.featurize import ALPHABET
 from proteinmpnn.models import load_abmpnn, load_model, load_proteinmpnn
 from proteinmpnn.sample import sample
-from proteinmpnn.score import ScoringResult, score_sequences
+from proteinmpnn.score import ScoringResult, score
+from proteinmpnn.featurize import featurize_structure, encode_sequence
+from proteinmpnn.io import parse_pdb
 
 __all__ = [
     "load_model",
     "load_proteinmpnn",
     "load_abmpnn",
     "sample",
-    "score_sequences",
+    "score",
     "ScoringResult",
     "ALPHABET",
+    "featurize_structure",
+    "encode_sequence",
+    "parse_pdb",
 ]
